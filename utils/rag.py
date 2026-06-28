7305dcb81ca16064bebfd60c2c4776c9f049bc53
Shreya Pathak <pathakshreyab@gmail.com>
2026-06-28 22:20:44 +0530

    Update RAG logic

--- utils/rag.py
+++ utils/rag.py
@@ -22,7 +22,7 @@ vector_store = Chroma(
 # 🔹 Gemini LLM
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
-    google_api_key=os.getenv("GOOGLE_API_KEY"),
+    google_api_key=os.getenv("GOOGLE_API_KEY),
     temperature=0
 )
 
